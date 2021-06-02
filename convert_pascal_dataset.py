8d7c787df13e4e40e3e8dcb01a908036a44946ad
jimilee <jimi88lee@stu.kmu.ac.kr>
2021-06-02 20:25:42 +0900

    make pascal 2 yolo form

--- convert_pascal_dataset.py
+++ convert_pascal_dataset.py
@@ -3,8 +3,9 @@ from xml.etree.ElementTree import Element, dump, ElementTree
 import os
 #face 디텍션을 위한 PASCAL dataset -> YOLO format 가공.
 xml_path = "E:/TrainVal/VOCdevkit/VOC2011/Annotations/"
-img_path = "E:/TrainVal/VOCdevkit/VOC2011/JPEGImages/"
 txt_path = "E:/TrainVal/VOCdevkit/VOC2011/YoloAnnotations/"
+
+finding_label = {"person": 0, "dog": 3, "cat": 2, "chair": 5, "diningtable": 8, "sofa": 4, "tvmonitor": 7, "head":10}
 def convert_xml_2_txt():
     for xml_file in os.listdir(xml_path):
         print(xml_file)
@@ -18,7 +19,22 @@ def convert_xml_2_txt():
         img_height = int(img_size[0].find("height").text)
 
         print(img_width, img_height)
-        for tag in root.iter("part"):
+
+        for tag in root.iter("object"):
+            if tag.find("name").text in finding_label.keys():
+                xmin = int(tag.find("bndbox").findtext("xmin"))
+                ymin = int(tag.find("bndbox").findtext("ymin"))
+                xmax = int(tag.find("bndbox").findtext("xmax"))
+                ymax = int(tag.find("bndbox").findtext("ymax"))
+
+                # object label.
+                fw.write('{0} {1} {2} {3} {4}\n'.format(finding_label[tag.find("name").text],
+                                                        float((xmin + ((xmax - xmin) / 2)) / img_width),
+                                                        float((ymin + ((ymax - ymin) / 2)) / img_height),
+                                                        float((xmax - xmin) / img_width),
+                                                        float((ymax - ymin) / img_height)))
+
+        for tag in root.iter("part"): #person's part.
             if tag.find("name").text == "head":
                 xmin = int(tag.find("bndbox").findtext("xmin"))
                 ymin = int(tag.find("bndbox").findtext("ymin"))
@@ -33,10 +49,5 @@ def convert_xml_2_txt():
                                                         float((ymax - ymin) / img_height)))
 
         fw.close()
-
-
-
-
-
 
 convert_xml_2_txt()
