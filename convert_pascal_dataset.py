3ecc123c64e2db44a07c39b5a0eeb745d5d3f5bc
jimilee <jimi88lee@stu.kmu.ac.kr>
2021-06-02 20:11:36 +0900

    make pascal 2 yolo form

---
+++ convert_pascal_dataset.py
@@ -0,0 +1,42 @@
+import xml.etree.ElementTree as ET
+from xml.etree.ElementTree import Element, dump, ElementTree
+import os
+#face 디텍션을 위한 PASCAL dataset -> YOLO format 가공.
+xml_path = "E:/TrainVal/VOCdevkit/VOC2011/Annotations/"
+img_path = "E:/TrainVal/VOCdevkit/VOC2011/JPEGImages/"
+txt_path = "E:/TrainVal/VOCdevkit/VOC2011/YoloAnnotations/"
+def convert_xml_2_txt():
+    for xml_file in os.listdir(xml_path):
+        print(xml_file)
+        data = ET.parse(xml_path + xml_file)
+        root = data.getroot()
+        fw = open(txt_path + xml_file[:-4] + '.txt', 'a', encoding='utf-8')
+
+        #get image_size.
+        img_size = root.findall("size")
+        img_width = int(img_size[0].find("width").text)
+        img_height = int(img_size[0].find("height").text)
+
+        print(img_width, img_height)
+        for tag in root.iter("part"):
+            if tag.find("name").text == "head":
+                xmin = int(tag.find("bndbox").findtext("xmin"))
+                ymin = int(tag.find("bndbox").findtext("ymin"))
+                xmax = int(tag.find("bndbox").findtext("xmax"))
+                ymax = int(tag.find("bndbox").findtext("ymax"))
+
+                #face label.
+                fw.write('{0} {1} {2} {3} {4}\n'.format(10,
+                                                        float((xmin + ((xmax-xmin)/2)) / img_width),
+                                                        float((ymin + ((ymax-ymin)/2)) / img_height),
+                                                        float((xmax - xmin) / img_width),
+                                                        float((ymax - ymin) / img_height)))
+
+        fw.close()
+
+
+
+
+
+
+convert_xml_2_txt()
